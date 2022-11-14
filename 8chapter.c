74c10fdcea455af3703e8f791ab1b4ece8ed762c
ananya0901 <101514697+ananya0901@users.noreply.github.com>
2022-11-14 11:35:05 +0530

    Update 8chapter.c

--- 8chapter.c
+++ 8chapter.c
@@ -358,3 +358,63 @@ class gamma:public beta ,public alpha
   	g.show_mn();
   	return 0;
   }	
+
+
+8.8
+
+#include<iostream>
+using namespace std;
+class alpha
+{
+	int x;
+	public:
+		alpha(int i)
+		{
+			x=i;
+			cout<<"\n alpha constructor";
+		}
+		void show_alpha(void)
+		{
+			cout<<"x="<<x<<"\n";
+		}
+};
+class beta
+{
+	float p,q;
+	public:
+		beta(float a,float b):p(a),q(b+p)
+		{
+			cout<<"\n beta constructed";
+		}
+		void show_beta(void)
+		{
+			cout<<"p="<<p<<"\n";
+			cout<<"q="<<q<<"\n";
+		}
+};
+class gamma :public beta,public alpha
+{
+	int u,v;
+	public:
+		gamma(int a,int b,float c):
+			alpha(a*2),beta(c,c),u(a)
+			{
+				v=b;
+				cout<<"\n gamma constructed";
+			}
+		void show_gamma(void)
+		{
+			cout<<"u="<<u<<"\n";
+			cout<<"v="<<v<<"\n";
+		}
+};
+int main()
+{
+	gamma g(2,4,2.5);
+	cout<<"\n\n display member values"<<"\n\n";
+	g.show_alpha();
+	g.show_beta();
+	g.show_gamma();
+	
+	
+}
