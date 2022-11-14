55eb2d7ee1f3569c16b2a17f1e28001088879592
ananya0901 <101514697+ananya0901@users.noreply.github.com>
2022-11-14 11:06:09 +0530

    Update 8chapter.c

--- 8chapter.c
+++ 8chapter.c
@@ -223,3 +223,76 @@ int main()
 	student1.display();
 	return 0;
 }
+
+8.6:
+
+#include<iostream>
+using namespace std;
+class student
+{
+	protected:
+		int rool_number;
+		public:
+	   void get_number(int a)
+		{
+		rool_number=a;	
+		}
+		void put_number(void)
+		{
+			cout<<"ROLL NO:"<<rool_number<<"\n";
+		}
+};
+    class test:virtual public student
+    {
+    	protected:
+    		float part1,part2;
+    		public:
+    			void get_marks(float x,float y)
+    			{
+    				part1=x;
+    				part2=y;
+				}
+				void put_marks(void)
+				{
+					cout<<"marks obtained:"<<"\n"
+					<<"part1="<<part1<<"\n"
+					<<"part2="<<part2<<"\n";
+				}
+	};
+	class sports:public virtual student
+	{
+		protected:
+			float score;
+			public:
+				void get_score(float a)
+				{
+					score=a;
+				}
+				void put_score(void)
+				{
+					cout<<"sports wt:"<<score<<"\n\n";
+				}
+	};
+	class result :public test,public sports
+	{
+		float total;
+		public:
+			void display(void);
+	};
+	void result::display(void)
+	{
+		total=part1+part2+score;
+		put_number();
+		put_marks();
+		put_score();
+		cout<<"total score:"<<total<<"\n";
+	}
+	int main()
+	{
+		result student_1;
+		student_1.get_number(678);
+		student_1.get_marks(30.5,25.5);
+		student_1.get_score(7.0);
+		student_1.display();
+		return 0;
+	}
